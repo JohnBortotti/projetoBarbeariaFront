ab7dfe47069f77a7d1085e1581143bdde1163a4c
John Bortotti <joao_bortotti@hotmail.com>
2020-05-29 02:51:34 -0300

    add vue-router to project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import { BootstrapVue } from 'bootstrap-vue';
 import App from './App.vue';
+
+import Login from './components/Login';
+
 
 // Import the styles directly. (Or you could add them via script tags.)
 import 'bootstrap/dist/css/bootstrap.css';
@@ -9,7 +13,19 @@ import 'bootstrap-vue/dist/bootstrap-vue.css';
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue);
+Vue.use(VueRouter)
+
+const routes = [ // define as rotas e os componentes
+  { path: '/', component: Login }
+];
+
+const router = new VueRouter({ // instancia o router
+  routes,
+  mode: 'history'
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  render: h => h(App)
+})
