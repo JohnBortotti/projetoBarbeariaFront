331d31c251f782a358794a5ed665d63737a2182c
John Bortotti <joao_bortotti@hotmail.com>
2020-06-02 13:42:12 -0300

    removed bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import { BootstrapVue } from 'bootstrap-vue';
 import App from './App.vue';
 
 import Login from './components/Login/Login';
@@ -14,11 +13,9 @@ import ClientsUpdateForm from './components/Clients/ClientsUpdateForm/ClientsUpd
 import ClientsNewForm from './components/Clients/ClientsNewForm/ClientsNewForm';
 // Import the styles directly. (Or you could add them via script tags.)
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = false
 
-Vue.use(BootstrapVue);
 Vue.use(VueRouter)
 
 const routes = [ // define as rotas e os componentes
