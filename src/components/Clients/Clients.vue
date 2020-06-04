c6dda421b25a97e2e15b846a950e452f01d0e677
John Bortotti <joao_bortotti@hotmail.com>
2020-06-03 23:26:54 -0300

    new Clientes design

--- src/components/Clients/Clients.vue
+++ src/components/Clients/Clients.vue
@@ -1,68 +1,58 @@
 <template>
-  <div>
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-      <div class="navbar-brand text-light" >Barbearia</div>
-      <ul class="navbar-nav">
-        <li class="nav-item">
+  <div class="container">
+    <nav class="navbar">
+      <div class="navbar-items">
+        <div class="nav-item">
+          <img src="../../img/logo.png" id="logo" />
+        </div>
+        <div class="nav-item">
           <a class="nav-link" v-on:click="function(){$router.push('/home')}">Serviços</a>
-        </li>
-        <li class="nav-item active">
+        </div>
+        <div class="nav-item active">
           <a class="nav-link">Clientes</a>
-        </li>
-        <li class="nav-item">
+        </div>
+        <div class="nav-item quit">
           <a class="nav-link" v-on:click="logout()">Sair</a>
-        </li>
-      </ul>
+        </div>
+      </div>
     </nav>
-    <h4 class="p-5 text-center text-secondary">Clientes</h4>
-    <div class="row justify-content-md-center">
-      <!--- button center wrapper -->
+    <div class="title-div">CLIENTES</div>
+    <div class="buttons-row">
       <button
         type="button"
         v-on:click="function(){$router.push('/Clientsnewform')}"
         id="updateClientButton"
-        class="btn btn-primary col-2 mb-4 mr-2"
+        class="table-button"
       >Novo Cliente</button>
       <button
         type="button"
         v-on:click="function(){$router.push('/clientsupdateform')}"
         id="newClientButton"
-        class="btn btn-info col-2 mb-4"
+        class="table-button"
       >Atualizar Cliente</button>
     </div>
-    <div class="row justify-content-md-center">
-      <div
-        v-if="fail == true"
-        class="alert alert-danger w-50"
-        role="alert"
-      >Não é possivel deletar clientes com serviços registrados!</div>
-    </div>
-    <div v-if="clients.length == 0" class="d-flex justify-content-center">
-      <div class="spinner-border" role="status">
-        <span class="sr-only">Loading...</span>
-      </div>
-    </div>
-    <div v-else class="table-responsive-sm">
-      <table class="table w-75 mx-auto table-dark">
+    <div v-if="fail == true" class="error">Não é possivel deletar clientes com serviços registrados!</div>
+    <div class="table-div">
+      <table class="main-table">
         <thead>
           <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Nome</th>
-            <th scope="col">Telefone</th>
-            <th scope="col">Email</th>
-            <th scope="col">Controles</th>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Telefone</th>
+            <th>Email</th>
+            <th>Controles</th>
           </tr>
           <tr v-for="client in clients" :key="client.id" v-bind:id="client.id">
-            <th scope="col">{{ client.id }}</th>
-            <th scope="col">{{ client.name }}</th>
-            <th scope="col">{{ client.phone }}</th>
-            <th scope="col">{{ client.email }}</th>
-            <th scope="col">
-              <div>
+            <th>{{ client.id }}</th>
+            <th>{{ client.name }}</th>
+            <th>{{ client.phone }}</th>
+            <th>{{ client.email }}</th>
+            <th>
+              <div class="table-controlers-div">
                 <button
                   v-on:click.prevent="deleteClient(client.id)"
                   type="button"
-                  class="btn btn-sm btn-danger"
+                  class="table-controler"
                   style="margin-right: 10px"
                 >Deletar</button>
               </div>
@@ -134,26 +124,155 @@ export default {
 </script>
 
 <style scoped>
-div {
-  max-width: 100%;
+.container {
+  display: flex;
+  height: 100%;
+  color: white;
+  background: url("../../img/skull.png") rgb(17, 16, 16);
+  background-position: start;
+  background-repeat: no-repeat;
+  background-blend-mode: multiply;
+  background-size: 600px;
+  flex-direction: column;
+  align-items: center;
 }
-a {
+
+.navbar {
+  display: flex;
+  background-color: rgb(22, 22, 26);
+  width: 100%;
+  height: 8%;
+}
+
+#logo {
+  width: 40px;
+}
+
+.navbar-items {
+  cursor: pointer;
+  font-family: "Poppins", sans-serif;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  color: white;
+}
+
+.nav-item {
+  margin: 0 10px;
+}
+
+.active {
+  cursor: default;
+  color: #7f5e35;
+}
+.quit {
+  background-color: #a07541;
+  padding: 0 25px;
+  border-radius: 20px;
+}
+
+.error {
+  color: rgb(207, 89, 89);
+  margin-bottom: 10px;
+  font-family: "Poppins", sans-serif;
+}
+
+.title-div {
+  font-family: "Smythe", cursive;
+  display: flex;
+  font-weight: 600;
+  align-items: center;
+  margin-bottom: 40px;
+  width: 90%;
+  height: 10%;
+  font-size: 40px;
+}
+.buttons-row {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+}
+
+.table-button {
+  transition: 0.3s;
+  margin-right: 20px;
+  margin-bottom: 50px;
+  width: 160px;
+  padding: 12px 18px;
+  background-color: rgba(0, 0, 0, 0);
+  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+  font-size: 16px;
+  color: #a07541;
+  border: 2px solid #a07541;
   cursor: pointer;
 }
 
-@media screen and (max-width: 800px) {
-  .row {
-    margin: 0px;
-    width: 100% !important;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+.table-button:hover {
+  background-color: #a07541;
+  color: white;
+}
+
+.table-div {
+  border: 2px solid rgb(105, 85, 48);
+  width: 80%;
+  padding: 6px;
+  overflow: scroll;
+  margin-bottom: 20px;
+}
+
+.main-table {
+  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+  width: 100%;
+  font-size: 17px;
+  border-collapse: collapse;
+  color: white;
+}
+
+thead {
+  font-size: 18px;
+}
+
+th,
+tr {
+  border-bottom: 1px solid #ddd;
+  height: 50px;
+}
+
+.table-controler {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  background-color: rgba(0, 0, 0, 0);
+  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+  cursor: pointer;
+  color: white;
+  border: 1px solid white;
+  border: solid;
+  width: 90px;
+  height: 35px;
+  margin: 0 6px;
+  transition: 0.3s;
+}
+
+.table-controler:hover {
+  background-color: rgb(129, 96, 35);
+  color: black;
+}
+
+@media screen and (max-width: 768px) {
+  .table-button {
+    margin: 20px 5px;
   }
-  #updateClientButton,
-  #newClientButton {
-    max-width: 80% !important;
-    margin: 0px !important;
-    margin-bottom: 8px !important;
+
+  .error {
+    text-align: center;
+  }
+}
+
+@media screen and (max-width: 400px) {
+  .navbar {
+    height: 20%;
+  }
+  .navbar-items {
+    display: inline-block;
   }
 }
 </style>
--- src/components/Clients/ClientsNewForm/ClientsNewForm.vue
+++ src/components/Clients/ClientsNewForm/ClientsNewForm.vue
@@ -2,38 +2,27 @@
   <div class="container">
     <button
       type="button"
-      class="btn btn-danger col-1 p-1 m-4"
+      class="back-button"
       v-on:click="function(){$router.push('/clients')}"
     >Voltar</button>
-    <div class="row justify-content-sm-center">
-      <form class="w-25" @submit.prevent="updateClient(name, phone, email)">
-        <h3 class="mb-5 text-center">Novo Cliente</h3>
-        <div class="form-group">
+    <h3 class="title">Novo Cliente</h3>
+    <div class="form-div">
+      <form class="form" @submit.prevent="updateClient(name, phone, email)">
+        <div class="form-input">
           <label>Nome</label>
-          <input class="form-control" v-model="name" placeholder="insira o nome" required />
+          <input class="form-input input" v-model="name" placeholder="insira o nome" required />
         </div>
-        <div class="form-group">
+        <div class="form-input">
           <label>Telefone</label>
-          <input class="form-control" v-model="phone" placeholder="insira o telefone" required />
+          <input class="form-input input" v-model="phone" placeholder="insira o telefone" required />
         </div>
-        <div class="form-group">
+        <div class="form-input">
           <label>Email</label>
-          <input class="form-control" v-model="email" placeholder="insira o email" required />
+          <input class="form-input input" v-model="email" placeholder="insira o email" required />
         </div>
-        <div class="row justify-content-md-center">
-          <!-- button wrapper para centralizar o button na div -->
-          <button class="btn btn-primary col-6 m-3">Cadastrar</button>
-          <div
-            v-if="success == true"
-            class="alert alert-success text-center"
-            role="alert"
-          >Cliente Cadastrado</div>
-          <div
-            v-if="fail == true"
-            class="alert alert-danger text-center"
-            role="alert"
-          >Falha ao cadastrar cliente</div>
-        </div>
+        <button class="form-button">Cadastrar</button>
+        <div v-if="success == true" class="success">Cliente Cadastrado</div>
+        <div v-if="fail == true" class="error">Falha ao cadastrar cliente</div>
       </form>
     </div>
   </div>
@@ -62,15 +51,15 @@ export default {
         body: JSON.stringify({
           name: name,
           phone: phone,
-          email: email,
+          email: email
         })
       }).then(res => {
         if (res.status != 201) {
           this.fail = true;
           this.success = false;
         } else {
-            this.success = true;
-            this.fail = false;
+          this.success = true;
+          this.fail = false;
         }
       });
     }
@@ -79,22 +68,106 @@ export default {
 </script>
 
 <style scoped>
-div {
-  max-width: 100%;
-  max-height: 100% !important;
-  background-color: rgb(247, 247, 247);
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: rgb(17, 16, 16);
+  height: 100%;
+  background: url("../../../img/skull.png") rgb(17, 16, 16);
+  background-position: start;
+  background-repeat: no-repeat;
+  background-blend-mode: multiply;
+  background-size: 600px;
 }
 
-@media screen and (max-width: 700px) {
-  form {
-    margin: 0 10%;
-  }
-  .w-25 {
-    width: 100% !important;
-  }
+.back-button {
+  margin: 40px 60px;
+  align-self: flex-start;
+  background-color: #a07541;
+  padding: 10px 38px;
+  border-radius: 20px;
+  border: none;
+  color: white;
+  cursor: pointer;
+  transition: 0.3s;
 }
-.btn-danger {
-  width: 100px !important;
-  max-width: 100% !important;
+
+.back-button:hover {
+  background-color: #be8f54;
+}
+
+.title {
+  color: white;
+  font-family: "Smythe", cursive;
+  font-size: 3em;
+  margin-bottom: 20px;
+  border-bottom: 1px solid white;
+}
+
+.form-div {
+  width: 60%;
+  height: 50%;
+}
+
+.form {
+  margin-top: 40px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: sans-serif;
+  font-size: 16px;
+}
+
+label {
+  margin-right: 20px;
+}
+
+.form-input {
+  font-family: "Smythe", cursive;
+  font-size: 24px;
+  color: white;
+  margin: 10px 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: rgba(17, 16, 16, 0);
+}
+
+.input {
+  font-family: "Smythe", cursive;
+  font-size: 18px;
+  background-color: rgb(17, 16, 16);
+  border: 2px solid white;
+  padding: 4px 18px;
+  border-radius: 4px;
+}
+
+.form-button {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 16px;
+  cursor: pointer;
+  margin-top: 30px;
+  padding: 10px 64px;
+  border: none;
+  border-radius: 8px;
+  transition: 0.3s;
+}
+
+.form-button:hover {
+  padding: 10px 74px;
+}
+
+.error {
+  margin-top: 20px;
+  color: rgb(223, 70, 70);
+  font-family: "Poppins", sans-serif;
+}
+
+.success {
+  margin-top: 20px;
+  color: rgb(112, 218, 86);
+  font-family: "Poppins", sans-serif;
 }
 </style>
